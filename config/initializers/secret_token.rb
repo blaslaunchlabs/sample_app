0ae2563b6eca8ebaa0028e0b2f232f9e7fbbf73d
blaslaunchlabs <blas_delossantos@launchlabs.ph>
2016-02-01 13:49:40 +0800

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '612816bc1adc35275ac82959b490e91c9222b703700a7f20a3db8460243ba43bfe565d2cc1f9911b45f43437f20d05a5e2cef0ac77a05878000e61287719a8a1'
